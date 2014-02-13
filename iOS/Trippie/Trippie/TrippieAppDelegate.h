5bb5771ba078f8e80a552f3fc313a82f89bf1a25
lewkoo <lewkoo@gmail.com>
2014-02-13 15:06:27 -0600

    iOS empty project added
    nothing there yet

---
+++ iOS/Trippie/Trippie/TrippieAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TrippieAppDelegate.h
+//  Trippie
+//
+//  Created by Levko Ivanchuk on 2/13/2014.
+//  Copyright (c) 2014 Group 2 COMP 4350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrippieAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/Trippie/TrippieTests/TrippieTests.m
@@ -0,0 +1,34 @@
+//
+//  TrippieTests.m
+//  TrippieTests
+//
+//  Created by Levko Ivanchuk on 2/13/2014.
+//  Copyright (c) 2014 Group 2 COMP 4350. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrippieTests : XCTestCase
+
+@end
+
+@implementation TrippieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
