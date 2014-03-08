e4a029f86df7c473e7fd8738be1d8d9b8a0a5d3f
lewkoo <lewkoo@gmail.com>
2014-03-08 12:30:10 -0600

    Added project stub

---
+++ iOS/SimpleWeather/SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Levko Ivanchuk on 2014-03-08.
+//  Copyright (c) 2014 Levko Ivanchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
