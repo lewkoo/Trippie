7a44b25c81d582952d984ed0c4a50910eb8e9c05
umwalk69 <umwalk69@myumanitoba.ca>
2014-03-09 16:46:11 -0500

    Adding angular controller and service for lodgings

---
+++ public/js/services/lodgings.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Lodgings service used for lodgings REST endpoint
+angular.module('trippie.lodgings').factory('Lodgings', ['$resource', function($resource) {
+    return $resource('/trips/:tripId/destinations/:destinationId/lodgings/:lodgingId', {
+        lodgingId: '@_id'
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    });
+}]);
