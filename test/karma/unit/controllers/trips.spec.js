58d0c93774d5fa408f39c82999b88cb568923430
Jordan Wood <jordanmyleswood@gmail.com>
2014-03-11 20:48:58 -0500

    Created skeletons for testing trip and destination

---
+++ test/karma/unit/controllers/trips.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function() {
+    // Trips Controller Spec
+    describe('Trippie controllers', function() {
+        describe('TripsController', function() {
+            // The $resource service augments the response object with methods for updating and deleting the resource.
+            // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
+            // the responses exactly. To solve the problem, we use a newly-defined toEqualData Jasmine matcher.
+            // When the toEqualData matcher compares two objects, it takes only object properties into
+            // account and ignores methods.
+            beforeEach(function() {
+                this.addMatchers({
+                    toEqualData: function(expected) {
+                        return angular.equals(this.actual, expected);
+                    }
+                });
+            });
+
+            // Load the controllers module
+            beforeEach(module('trippie'));
+
+            // Initialize the controller and a mock scope
+            var TripsController,
+                scope,
+                $httpBackend,
+                $routeParams,
+                $location;
+
+            // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+            // This allows us to inject a service but then attach it to a variable
+            // with the same name as the service.
+            beforeEach(inject(function($controller, $rootScope, _$location_, _$routeParams_, _$httpBackend_) {
+                scope = $rootScope.$new();
+
+                TripsController = $controller('TripsController', {
+                    $scope: scope
+                });
+
+                $routeParams = _$routeParams_;
+
+                $httpBackend = _$httpBackend_;
+
+                $location = _$location_;
+            }));
+
+        });
+    });
+}());
