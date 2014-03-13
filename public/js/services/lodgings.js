1f69001d46b16126a6e8ca4185ca35110a1c6989
Chris Ventura <chrisventura@live.ca>
2014-03-13 08:59:08 -0500

    Fixed some small JShint errors

--- public/js/services/lodgings.js
+++ public/js/services/lodgings.js
@@ -2,11 +2,10 @@
 
 //Lodgings service used for lodgings REST endpoint
 angular.module('trippie.lodgings').factory('Lodgings', ['$resource', function($resource) {
-
     return $resource('trips/:tripId/destinations/:destinationId/lodgings', null,
-	    {
-	        update: {
-	            method: 'PUT'
-	        }
-	    });
+    {
+		update: {
+			method: 'PUT'
+		}
+    });
 }]);
--- public/js/services/notes.js
+++ public/js/services/notes.js
@@ -2,11 +2,10 @@
 
 //Events service used for events REST endpoint
 angular.module('trippie.notes').factory('Notes', ['$resource', function($resource) {
-
     return $resource('trips/:tripId/destinations/:destinationId/notes', null,
-	    {
-	        update: {
-	            method: 'PUT'
-	        }
-	    });
+	{
+		update: {
+			method: 'PUT'
+		}
+	});
 }]);
